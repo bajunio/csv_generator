9695cc67bf38b7afa69ef4f50329b032432d69a7
David Campbell <david@mrcampbell.org>
2015-10-17 16:41:25 -0700

    Usually, initializing with options will prevent future pain when API changes are made.  Sometimes it's not always the best option though, like when you want to force require certain parameters to exist.  Even then, I'd recommend including an final hash argument.
    Always try to sanitize and validate arguments, especially when they
    comes from humans.  That way files don't get "lost" when someone tries
    ` --output /tmp/foo/out.csv` they aren't surprised when it ends up in
    `./csv_generator_output/tmp/foo/out.csv`.
    
    Now, if you try to run this code, you get an error.
    
    ```
      $ ./csv_generator.rb output 4 1
      Missing argument: out,rows,cols
      Usage: ./csv_generator.rb <output_file> <number_of_columns> <number_of_rows>
    ```
    
    What the hell dude!?  I'll address it in the next commit, gunna try to
    break things down into smaller chunks from now on.

--- csv_generator.rb
+++ csv_generator.rb
@@ -2,16 +2,43 @@
 
 class CSVGenerator
 
-  def initialize
-    @output_file = ARGV[0]
-    @number_of_columns = ARGV[1].to_i
-    @number_of_rows = ARGV[2].to_i
-    @output_directory = "#{File.basename($0, ".*")}_output"
-    self.argument_check
+  ##
+  # Pass in a hash as args
+  # Alternatively you could set specific args, like
+  #
+  #   `initialize(output_file, column_count, row_count)`
+  #
+  # but that would also limit flexibility in the future to make
+  # changes to the API.
+  def initialize(opts={})
+    # Validate arguments first, there's no need to proceed if things
+    # aren't gunna work anyways.
+    #
+    # I've decided to rename `#argument_check` to `#validate_args`.
+    # Why? Well, it sounds more idomatic. Rails calls it's parameter check
+    # classes `Validators` for example.
+    #
+    # Also want to briely 'explain thy `self`'. There's usually no
+    # need the call `self.method` inside of instance methods.  All of
+    # your method calls are already scoped to that particular instance.
+    validate_args(opts)
+
+    @output_file       = opts[:out]
+    @number_of_columns = opts[:cols].to_i
+    @number_of_rows    = opts[:rows].to_i
+    @output_directory  = "#{File.basename($0, ".*")}_output"
   end
 
+  # So now that we aren't sourcing ARGV for parameters and we're redefined
+  # how we call `CSVGenerator.new`, I'm going to deprecate this method.
+  #
+  # Since `#argument_check` was called inside of initialize, users
+  # would rarely have called it, if ever, but because it was defined as a public method
+  # I'm going to simple deprecate it until the next release.
+  #
   def argument_check
-    (ARGV.size != 3) ? prompt_user : run_the_works
+    warn "[DEPRECATION] `argument_check` is deprecated and will be removed in future release"
+    #   (ARGV.size != 3) ? prompt_user : run_the_works
   end
 
   def prompt_user
@@ -81,6 +108,52 @@ class CSVGenerator
     self.write_newline
     self.write_rows
     self.exit_text
+  end
+
+  # Let's cycle through the keys we require and select those key
+  # values equal nil in the opts hash.  Essentially:
+  #
+  #     required_args = [:out, :rows, :cols]
+  #     missing = []
+  #     required_args.each do |sym|
+  #       if opts[sym].nil?
+  #         missing << :out
+  #       end
+  #     end
+  #
+  # Then, if `missing.any?` we know that some arguments were not
+  # passed and we should raise an exception.  Rather than do all that
+  # here, we'll call instance method `#usage` and pass it the missing args.
+  def validate_args(opts={})
+    return if (missing = [:out, :rows, :cols].select{|a| opts[a].nil? }).empty?
+    usage(missing)
+  end
+
+
+  # Params are an array of missing parameters passed as a splat (*).
+  # All splat does is says "you can pass me as any individual
+  # arguments as your want and I'll pretend like you passed me an
+  # array instead".  Here's an illustration:
+  #
+  #    usage_args(:out, :cols, :rows)
+  #
+  #    def usage_args(arg1, arg2, arg3)
+  #        arg4 = :format
+  #        usagesplat(arg1, arg2, arg3, arg4)
+  #    end
+  #
+  #    def usage_splat(*args)
+  #        puts args.join(', ')
+  #        # :out, :cols, :rows, :format
+  #    end
+  #
+  # By calling exit 1, you're letting users know that there was an error.
+  def usage(*args)
+    if args.any?
+      puts 'Missing argument' + (args.size > 1 ? 's' : '') + ': ' + args.join(',')
+    end
+    puts "Usage: #{$0} <output_file> <number_of_columns> <number_of_rows>"
+    exit 1
   end
 
 end
